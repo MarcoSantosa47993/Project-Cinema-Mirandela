71ce19f6e963337e54a0811b9719431d726907a2
MarcoSantosa47993 <a47993@alunos.ipb.pt>
2024-04-21 18:53:16 +0100

    message

--- servidor/models/bilheteModel.js
+++ servidor/models/bilheteModel.js
@@ -1,34 +1,33 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose'); // Importing Mongoose library for MongoDB interaction
 
+// Defining the schema for the "bilhetes" collection
 const bilheteSchema = new mongoose.Schema({
-
     sessao: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'sessoes'
+        ref: 'sessoes' // Reference to the "sessoes" collection
     },
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'users'
+        ref: 'users' // Reference to the "users" collection
     },
     lugares: {
         type: Array,
-        required: true,
+        required: true, // Array of seat numbers or locations, required
     },
     pagamentoId: {
         type: String,
-        required: false,
+        required: false, // ID of the payment transaction, not required
     },
     estado: {
         type: String,
         required: true,
-        default: "Comprado"
+        default: "Comprado" // State of the ticket, default value is "Comprado" (Purchased)
     },
     dataHoraValidacao: {
-        type: Date, // Armazene a data e a hora da validação
-        required:false
+        type: Date,
+        required: false // Date and time of validation, not required
     },
+}, { timestamps: true }); // Automatic timestamps for creation and modification
 
-}, {timestamps: true})
-
-
-module.exports = mongoose.model("bilhetes", bilheteSchema) 
+// Exporting the Mongoose model for the "bilhetes" collection
+module.exports = mongoose.model("bilhetes", bilheteSchema);
