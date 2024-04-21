b1ffb4fbae6ac28cfcdff75a4d4fe47fc343c399
MarcoSantosa47993 <a47993@alunos.ipb.pt>
2024-04-21 18:44:41 +0100

    message

--- servidor/config/dbconfig.js
+++ servidor/config/dbconfig.js
@@ -1,21 +1,25 @@
-const mongoose = require('mongoose');
-const CryptoJS = require('crypto-js');
-const SECRET_KEY = "c5e2bf6c88a6e93b369880f1bca2d937a0b32aa5d5ed30eb9dea1df56b4a2e43";
+// Importing required modules
+const mongoose = require('mongoose'); // Importing Mongoose library for MongoDB connection
+const CryptoJS = require('crypto-js'); // Importing CryptoJS library for encryption
+const SECRET_KEY = "c5e2bf6c88a6e93b369880f1bca2d937a0b32aa5d5ed30eb9dea1df56b4a2e43"; // Predefined secret key for encryption
 
+// Function to decrypt ciphertext using AES encryption
 function decrypt(ciphertext) {
-    const bytes = CryptoJS.AES.decrypt(ciphertext, SECRET_KEY);
-    return bytes.toString(CryptoJS.enc.Utf8);
+    const bytes = CryptoJS.AES.decrypt(ciphertext, SECRET_KEY); // Decrypting ciphertext using AES and the secret key
+    return bytes.toString(CryptoJS.enc.Utf8); // Converting decrypted bytes to UTF-8 encoded string
 }
 
-// Uso da função decrypt
-const decryptedMongoUrl = decrypt(process.env.mongo_url);
-mongoose.connect(decryptedMongoUrl)
-const connection = mongoose.connection;
+// Usage of the decrypt function
+const decryptedMongoUrl = decrypt(process.env.mongo_url); // Decrypting MongoDB connection URL from environment variable
+mongoose.connect(decryptedMongoUrl); // Connecting to MongoDB using decrypted URL
+const connection = mongoose.connection; // Creating MongoDB connection object
 
+// Event listener for MongoDB connection established
 connection.on('connected', () => {
-    console.log('Mongo DB connection established')
-})
+    console.log('Mongo DB connection established'); // Logging success message when connection is established
+});
 
+// Event listener for MongoDB connection error
 connection.on('error', (err) => {
-    console.log('Mongo DB connection failed', err)
-})
+    console.log('Mongo DB connection failed', err); // Logging error message when connection fails
+});
--- servidor/middlewares/authMiddleware.js
+++ servidor/middlewares/authMiddleware.js
@@ -1,12 +1,14 @@
-const { response } = require("express");
-const jwt = require("jsonwebtoken");
 
+const { response } = require("express"); // Import response object from Express
+const jwt = require("jsonwebtoken"); // Import jsonwebtoken for handling JWT
+
+// Middleware function to verify JWT token
 module.exports = function (req, res, next) {
     try {
-        const token = req.headers.authorization.split(" ")[1];
-        const decoded = jwt.verify(token, process.env.jwt_secret);
-        req.body.userId = decoded.userId;
-        next();
+        const token = req.headers.authorization.split(" ")[1]; // Extract token from Authorization header
+        const decoded = jwt.verify(token, process.env.jwt_secret); // Verify and decode the token
+        req.body.userId = decoded.userId; // Attach decoded user ID to the request body
+        next(); // Move to the next middleware or route handler
     } catch (error) {
         res.status(401).send({ success: false, message: "invalid token" })
     }
