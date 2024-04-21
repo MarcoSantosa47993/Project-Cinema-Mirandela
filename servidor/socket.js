816232260383ecaf4c2e5243770774a6c1502e37
MarcoSantosa47993 <a47993@alunos.ipb.pt>
2024-04-21 18:38:41 +0100

    message

--- servidor/socket.js
+++ servidor/socket.js
@@ -1,12 +1,13 @@
-const { Server } = require('socket.io');
+const { Server } = require('socket.io'); // Import the Server class from socket.io
 
+// Create a new instance of Server with specific configuration
 const io = new Server({
   cors: {
-    origin: ["https://cinema-mirandela2.onrender.com"],  // Permite a conexão apenas destes IPs
-    methods: ["GET", "POST"],
-    allowedHeaders: ["my-custom-header"],
-    credentials: true
+    origin: ["https://cinema-mirandela2.onrender.com"], // Allow connection only from these origins
+    methods: ["GET", "POST"], // Allow specific HTTP methods
+    allowedHeaders: ["my-custom-header"], // Allow specific custom headers
+    credentials: true // Allow credentials to be sent with requests
   }
 });
 
-module.exports = io;
+module.exports = io; // Export the configured socket.io instance
